3be88056e00fb3e19010dbf5004935ad289f32aa
Boladek <bmubarak88@gmail.com>
2023-06-29 00:06:36 +0100

    added gradient background

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { GradientBackgroundContainer } from "../../components/quote-generator/QuoteGeneratorElements";
+
+export default function Home() {
+  return (
+    <GradientBackgroundContainer>
+      Dynamic background Container
+    </GradientBackgroundContainer>
+  );
+}
